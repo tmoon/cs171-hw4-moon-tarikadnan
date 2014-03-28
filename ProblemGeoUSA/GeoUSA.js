d8273795d68ed34cadffbccad44e3d5bdd348a5e
Tarik Adnan Moon <tmoon@college.harvard.edu>
2014-03-27 23:24:34 -0400

    done

--- ProblemGeoUSA/GeoUSA.js
+++ ProblemGeoUSA/GeoUSA.js
@@ -49,14 +49,14 @@ var dataSet = {};
 
 var centered;
 
+// global array for the x-axis
 var xaxis_vals = ["01:00:00 AM", "02:00:00 AM", "03:00:00 AM", "04:00:00 AM", "05:00:00 AM", "06:00:00 AM", "07:00:00 AM", "08:00:00 AM", "09:00:00 AM", "10:00:00 AM", "11:00:00 AM", "12:00:00 PM", "01:00:00 PM", "02:00:00 PM", "03:00:00 PM", "04:00:00 PM", "05:00:00 PM", "06:00:00 PM", "07:00:00 PM", "08:00:00 PM", "09:00:00 PM", "10:00:00 PM", "11:00:00 PM", "12:00:00 AM"];
+//scales
 var x = d3.scale.ordinal().domain(xaxis_vals).rangePoints([detail_par.x, detail_par.w - 3 * detail_par.x]),
 y = d3.scale.linear().range([detail_par.h-75, detail_par.y]);
 
-
+// axes
 var yAxis = d3.svg.axis().scale(y).orient("right");
-
- 
 var xAxis = d3.svg.axis().scale(x)
 // .tickValues(x.domain().filter(function(d, i) { return !(i % 2); }))
 .orient("bottom");
@@ -65,6 +65,8 @@ var xAxis = d3.svg.axis().scale(x)
 function loadStations(val_data) {
 
     d3.csv("../data/NSRDB_StationsMeta.csv",function(error,data){
+        var id;
+        // this function find the correct radius
         function radius(d){
             id = String(d.USAF);
             if (val_data[id] != null)
@@ -75,6 +77,7 @@ function loadStations(val_data) {
             return 2;
         }
         
+        // this function gets classes
         function get_class(d){
             id = String(d.USAF);
             if (val_data[id] != null)
@@ -86,17 +89,19 @@ function loadStations(val_data) {
                 return "station";
             
         }
+
         var names = Object.keys(val_data);
         var domain_dt =[];
         for (var i = 0; i < names.length; i++) {
             domain_dt.push(val_data[names[i]].sum);
         };
+
         var scale = d3.scale.linear().domain(d3.extent(domain_dt.filter(function(d){return d>0;}))).range([2,8]);
-        var id;
         var y_max_val = d3.max(domain_dt);
 
         createDetailVis(y_max_val, val_data[names[0]].hourly);
 
+        // first make a canvas at the right position
         var circles = svg.selectAll(".station")
             .data(data)
             .enter().append("g")
@@ -108,33 +113,23 @@ function loadStations(val_data) {
                   d["NSRDB_LAT (dd)"]
                 ]) + ")"
               });
-
-        // circles
-        //     .append("rect")
-        //     .attr("class", "textbox")
-        //     .attr("width",100)
-        //     .attr("height",30)
-        //     .attr("transform","translate(10,-10)")
-        //     .style("visibility", "hidden")
-
+        // add tooltip texts
         circles
             .append("text")   
-            // .style("z-index", "10")
             .attr("transform","translate(15,0)")
-            // .style("dx", ".1em")
             .style("visibility", "hidden")
             .html(function(d){
                 if (get_class(d) != 'station')
                     return d.STATION +", "+val_data[String(d.USAF)].sum;
                 else
                     return d.STATION+", NO DATA";
-
             });
-
+        // add circles
         circles.attr("class", get_class).append("circle")
             // .attr("class", get_class)
             .attr("r",radius);
-           
+        
+        // handles mouse events
         svg.selectAll(".hasData")
             .on('mouseover',   function(d){
                     d3.select(this).style("z-index", "-1");
@@ -146,7 +141,6 @@ function loadStations(val_data) {
                 d3.select(this).style("z-index", "10");
                 d3.select(this).select("text").style("visibility", "hidden");
             });
-
     });
 }
 
@@ -156,9 +150,6 @@ function loadStats() {
     d3.json("../data/reducedMonthStationHour2003_2004.json", function(error,data){
         completeDataSet= data;
 
-		//....
-        // console.log(data);
-		
         loadStations(completeDataSet);
     })
 
@@ -190,13 +181,12 @@ function clicked(d) {
   svg.transition()
       .duration(500)
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      // .style("stroke-width", 1.5 / k + "px");
 }
 
+// load map
 d3.json("../data/us-named.json", function(error, data) {
 
     var usMap = topojson.feature(data,data.objects.states).features
-    // console.log(usMap);
 
     svg.selectAll(".country")
         .data(usMap)
@@ -216,8 +206,9 @@ function trans_bars(d,i){
     var time = xaxis_vals[i];
     return "translate(" + x(time) + "," + y(d) + ")"; 
 }
-// ALL THESE FUNCTIONS are just a RECOMMENDATION !!!!
+
 var allBars;
+
 var createDetailVis = function(y_max, base_data){
     y.domain([0, 14000000]);
 
@@ -244,22 +235,6 @@ var createDetailVis = function(y_max, base_data){
                 return "rotate(-65)" 
             });
     allBars = detailVis.append("g");
-    // updateDetailVis(base_data,"name");
-    // var data = [];
-    // for (var i = 0; i < xaxis_vals.length; i++) {
-    //     data.push(base_data[xaxis_vals[i]]);
-    // };
-
-    // var bar = detailVis.append("g").selectAll(".bar")
-    //     .data(data)
-    //   .enter().append("g")
-    //     .attr("class", "bar")
-    //     .attr("transform", trans_bars);
-
-    // bar.append("rect")
-    //     .attr("x", 0)
-    //     .attr("width", 8)
-    //     .attr("height", function(d,i) { console.log(d); return detail_par.h - y(d) - 3 * detail_par.x; });
 }
 
 
@@ -268,7 +243,6 @@ var updateDetailVis = function(given_data, name){
     for (var i = 0; i < xaxis_vals.length; i++) {
         data.push(given_data[xaxis_vals[i]]);
     };
-    console.log(data);
 
     detailVis.selectAll(".title")
         .text("Name: "+ name);
@@ -289,7 +263,5 @@ var updateDetailVis = function(given_data, name){
         .attr("height", function(d,i) {return detail_par.h - y(d) - 3 * detail_par.x; });
 
     bar.exit().remove();
-
-
 }
 
